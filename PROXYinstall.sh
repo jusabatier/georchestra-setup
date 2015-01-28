c1bd0340c0ef09e1cabf326c422b1fba53cc9ab9
jusabatier <julien.sabatier@agglo-lepuyenvelay.fr>
2015-01-28 09:11:15 +0100

    Add missing backslash before '$vars'

--- PROXYinstall.sh
+++ PROXYinstall.sh
@@ -53,7 +53,7 @@ server {
 			fastcgi_split_path_info ^(.+\.php)(/.+)$;
 			fastcgi_pass unix:/var/run/php5-fpm.sock;
 			fastcgi_index index.php;
-            fastcgi_param SCRIPT_FILENAME $document_root/$fastcgi_script_name;   
+        		fastcgi_param SCRIPT_FILENAME \$document_root/\$fastcgi_script_name;   
         }
 }
 EOT
