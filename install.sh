ff640138c7ba964b0edf98a0f5b556dac2945bcf
Julien Sabatier <julien.sabatier@agglo-lepuyenvelay.fr>
2015-10-14 12:24:48 +0200

    Update to TC8 and 15.06 release of Georchestra

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 SERVER_IP=$(hostname -I)
 SERVER_NAME="vm-jessie"
 
-GEORCHESTRA_VERSION="14.12"
+GEORCHESTRA_VERSION="15.06"
 
 POSTGRES_VERSION="9.4"
 POSTGIS_VERSION="2.1"
@@ -64,10 +64,8 @@ apt-get -y install openjdk-7-jdk libtcnative-1
 groupadd tomcat
 useradd -g tomcat -s /usr/sbin/nologin -m -d /home/tomcat tomcat
 
-# wget http://mir2.ovh.net/ftp.apache.org/dist/tomcat/tomcat-6/v6.0.43/bin/apache-tomcat-6.0.43.tar.gz -O /tmp/tomcat-6.tar.gz
-# wget http://mir2.ovh.net/ftp.apache.org/dist/tomcat/tomcat-7/v7.0.57/bin/apache-tomcat-7.0.57.tar.gz -O /tmp/tomcat-7.tar.gz
-cp $_base/tomcat-dist/apache-tomcat-6.0.43.tar.gz /tmp/tomcat-6.tar.gz
-cp $_base/tomcat-dist/apache-tomcat-7.0.61.tar.gz /tmp/tomcat-7.tar.gz
+# wget http://apache.mirrors.ovh.net/ftp.apache.org/dist/tomcat/tomcat-8/v8.0.27/bin/apache-tomcat-8.0.27.tar.gz /tmp/tomcat-8.tar.gz
+cp $_base/tomcat-dist/apache-tomcat-8.0.27.tar.gz  /tmp/tomcat-8.tar.gz
 
 mkdir /var/sig
 mkdir /var/sig/tmp_extracts
@@ -78,48 +76,48 @@ mkdir /var/sig/geowebcache_cachedir
 
 chown -R tomcat:tomcat /var/sig
 
-CUSTOMCAT_VERSION=6
+CUSTOMCAT_VERSION=8
 CUSTOMCAT_NAME="proxy"
 CUSTOMCAT_PORT=8080
 CUSTOMCAT_STOP=8005
 CUSTOMCAT_JAVAOPTS="-Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms256m -Xmx256m -XX:MaxPermSize=128m"
 . ./TOMCATinstall.sh
 
-CUSTOMCAT_VERSION=7
+CUSTOMCAT_VERSION=8
 CUSTOMCAT_NAME="cas"
 CUSTOMCAT_PORT=8081
 CUSTOMCAT_STOP=8015
 . ./TOMCATinstall.sh
 
-CUSTOMCAT_VERSION=7
+CUSTOMCAT_VERSION=8
 CUSTOMCAT_NAME="mapfish"
 CUSTOMCAT_PORT=8082
 CUSTOMCAT_STOP=8025
 CUSTOMCAT_JAVAOPTS="-Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms2G -Xmx2G -XX:MaxPermSize=256m"
 . ./TOMCATinstall.sh
 
-CUSTOMCAT_VERSION=7
+CUSTOMCAT_VERSION=8
 CUSTOMCAT_NAME="extractor"
 CUSTOMCAT_PORT=8083
 CUSTOMCAT_STOP=8035
 CUSTOMCAT_JAVAOPTS="-Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms2G -Xmx2G -XX:MaxPermSize=256m -Dorg.geotools.referencing.forceXY=true -Dextractor.storage.dir=/var/sig/tmp_extracts -Djava.util.prefs.userRoot=/tmp -Djava.util.prefs.systemRoot=/tmp"
 . ./TOMCATinstall.sh
 
-CUSTOMCAT_VERSION=6
+CUSTOMCAT_VERSION=8
 CUSTOMCAT_NAME="geonetwork"
 CUSTOMCAT_PORT=8084
 CUSTOMCAT_STOP=8045
 CUSTOMCAT_JAVAOPTS="-Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms2G -Xmx2G -XX:MaxPermSize=256m -Dgeonetwork.dir=/var/sig/geonetwork_datadir -Dgeonetwork.schema.dir=/var/sig/config/schema_plugins -Dgeonetwork.jeeves.configuration.overrides.file=/opt/tomcat-$CUSTOMCAT_NAME/webapps/geonetwork/WEB-INF/config-overrides-georchestra.xml -Djava.util.prefs.userRoot=/tmp -Djava.util.prefs.systemRoot=/tmp"
 . ./TOMCATinstall.sh
 
-CUSTOMCAT_VERSION=7
+CUSTOMCAT_VERSION=8
 CUSTOMCAT_NAME="utils"
 CUSTOMCAT_PORT=8085
 CUSTOMCAT_STOP=8055
 CUSTOMCAT_JAVAOPTS="-Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms2G -Xmx2G -XX:MaxPermSize=256m"
 . ./TOMCATinstall.sh
 
-CUSTOMCAT_VERSION=7
+CUSTOMCAT_VERSION=8
 CUSTOMCAT_NAME="geoserver"
 CUSTOMCAT_PORT=8086
 CUSTOMCAT_STOP=8065
